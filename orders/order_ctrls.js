2c6f0def15f244ec8d82868785e9cf62bfb8aacd
Chenfeng Liu <c.liu@binarystate.com>
2021-03-20 12:20:38 +0100

    added a PUT method for the order

--- orders/order_ctrls.js
+++ orders/order_ctrls.js
@@ -61,6 +61,56 @@ const createOrder = async (req, res) => {
 }
 
 
+
+const updateOrder = async (req, res) => {
+
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+
+    Order.findOne({_id: req.params._id } , (err, order) => {
+
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Order not found!',
+            })
+        }
+
+        if (!order) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Order not found` })
+        }
+
+        order.user_id = body.user_id
+        order.article = body.article
+        order
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: order._id,
+                    message: 'order updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'order update failed!',
+                })
+            })
+    })
+}
+
+
+
 const deleteOrder = async (req, res) => {
 
     await Order.findOneAndDelete({ _id: req.params._id }, (err, order) => {
@@ -115,6 +165,7 @@ const getOrders = async (req, res) => {
 module.exports = {
     createOrder,
     deleteOrder,
+    updateOrder,
     getOrderById,
     getOrders
 }
